2e18a67f2f90cd4be3463922e6870b9ebe69d617
Ternoviks <ternovik.daniil@gmail.com>
2022-02-23 14:07:01 +0200

    Izveidoti input fields un izvadas vards un vecums!

---
+++ Assets/Skripti/TekstaParadisana.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TekstaParadisana : MonoBehaviour {
+
+	public string teksts;
+	public string teksts2;
+	public GameObject ievadesLauks;
+	public GameObject ievadesLauks2;
+	public GameObject tekstaAttelosana;
+
+	public void uzglabaTekstu(){
+		teksts2 = ievadesLauks2.GetComponent<Text> ().text;
+		teksts = ievadesLauks.GetComponent<Text> ().text;
+		tekstaAttelosana.GetComponent<Text>().text = "Minecraft varonis "+teksts.ToUpper()+","+teksts2+" gadi!";
+	}
+}
