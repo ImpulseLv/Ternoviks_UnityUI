9b3f625f057513b296e971f2fc87d5e26e16c333
Ternoviks <ternovik.daniil@gmail.com>
2022-02-23 23:42:24 +0300

    Parveidotas bildes,izveidotas skanas klick un slider, un izlabots drop down

--- Assets/Skripti/AtteluParadisana.cs
+++ Assets/Skripti/AtteluParadisana.cs
@@ -7,13 +7,20 @@ public class AtteluParadisana : MonoBehaviour {
 
 	public GameObject CepuresPNG;
 	public GameObject CepuresPNG2;
+	public GameObject CepuresPNG3;
+	public GameObject CepuresPNG4;
 	public GameObject ZabakiPNG1;
 	public GameObject ZabakiPNG2;
+	public GameObject ZabakiPNG3;
+	public GameObject ZabakiPNG4;
 	public GameObject BiksesPNG1;
 	public GameObject BiksesPNG2;
+	public GameObject BiksesPNG3;
+	public GameObject BiksesPNG4;
 	public GameObject LadePNG1;
 	public GameObject LadePNG2;
-	public GameObject mainigaisAttels;
+	public GameObject LadePNG3;
+	public GameObject LadePNG4;
 	public Sprite[] atteluMasivs;
 
 	public void CepuresPNGparadisana (bool vertiba){
@@ -22,11 +29,23 @@ public class AtteluParadisana : MonoBehaviour {
 	public void CepuresPNG2paradisana (bool vertiba){
 		CepuresPNG2.SetActive (vertiba);
 	}
+	public void CepuresPNG3paradisana (bool vertiba){
+		CepuresPNG3.SetActive (vertiba);
+	}
+	public void CepuresPNG4paradisana (bool vertiba){
+		CepuresPNG4.SetActive (vertiba);
+	}
 	public void ZabakiPNG1paradisana (bool vertiba){
 		ZabakiPNG1.SetActive (vertiba);
 	}
 	public void ZabakiPNG2paradisana (bool vertiba){
 		ZabakiPNG2.SetActive (vertiba);
+	}
+	public void ZabakiPNG3paradisana (bool vertiba){
+		ZabakiPNG3.SetActive (vertiba);
+	}
+	public void ZabakiPNG4paradisana (bool vertiba){
+		ZabakiPNG4.SetActive (vertiba);
 	}
 	public void BiksesPNG1paradisana (bool vertiba){
 		BiksesPNG1.SetActive (vertiba);
@@ -34,11 +53,23 @@ public class AtteluParadisana : MonoBehaviour {
 	public void BiksesPNG2paradisana (bool vertiba){
 		BiksesPNG2.SetActive (vertiba);
 	}
+	public void BiksesPNG3paradisana (bool vertiba){
+		BiksesPNG3.SetActive (vertiba);
+	}
+	public void BiksesPNG4paradisana (bool vertiba){
+		BiksesPNG4.SetActive (vertiba);
+	}
 	public void LadePNG1paradisana (bool vertiba){
 		LadePNG1.SetActive (vertiba);
 	}
 	public void LadePNG2paradisana (bool vertiba){
 		LadePNG2.SetActive (vertiba);
+	}
+	public void LadePNG3paradisana (bool vertiba){
+		LadePNG3.SetActive (vertiba);
+	}
+	public void LadePNG4paradisana (bool vertiba){
+		LadePNG4.SetActive (vertiba);
 	}
 }
 
