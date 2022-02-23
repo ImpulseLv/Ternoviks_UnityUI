a100c220a92ee3990d66f5b5118693bb0bbb906e
Ternoviks <ternovik.daniil@gmail.com>
2022-02-24 00:58:43 +0300

    Projekts pabeigts!

--- Assets/Skripti/ObjektuParvietosana.cs
+++ Assets/Skripti/ObjektuParvietosana.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ObjektuParvietosana : MonoBehaviour {
+public class ObjektuParvietosana : MonoBehaviour, IPointerDownHandler, IDragHandler,IBeginDragHandler,IEndDragHandler {
 
-	// Use this for initialization
-	void Start () {
-		
+	private RectTransform transformacijuLogs;
+	public Canvas kanva;
+
+	private void Awake(){
+		transformacijuLogs = GetComponent<RectTransform> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void OnPointerDown(PointerEventData notikums){
+		Debug.Log ("Kreisais klikskis uz parvietojama objekta!");
 	}
+	public void OnDrag(PointerEventData notikums){
+		Debug.Log ("Objekts tiek parvietots!");
+		transformacijuLogs.anchoredPosition += notikums.delta / kanva.scaleFactor;
+	}
+	public void OnBeginDrag(PointerEventData notikums){
+		Debug.Log ("Uzsakta objekta parvietosana!");
+	}
+	public void OnEndDrag(PointerEventData notikums){
+		Debug.Log ("Pabeigta objekta parvietosana!");
+	}
+
 }
