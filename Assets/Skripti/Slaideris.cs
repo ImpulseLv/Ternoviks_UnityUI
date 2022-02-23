73c779f9f0ebffb77e3bcd198a5e62c8ed2dd272
Ternoviks <ternovik.daniil@gmail.com>
2022-02-24 00:40:58 +0300

    Izveidoti slaideri!

---
+++ Assets/Skripti/Slaideris.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slaideris : MonoBehaviour {
+
+	public GameObject slaideris;
+	public GameObject mainigaisAttels;
+
+	public void mainitGarumu(){
+		float pasreizejaVertiba = slaideris.GetComponent<Slider> ().value;
+		mainigaisAttels.transform.localScale = new Vector2 (1 ,1F * pasreizejaVertiba);
+	}
+}
---
+++ Assets/Skripti/Slaideris2.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slaideris2 : MonoBehaviour {
+
+	public GameObject slaideris;
+	public GameObject mainigaisAttels;
+
+	public void mainitPlatumu(){
+		float pasreizejaVertiba = slaideris.GetComponent<Slider> ().value;
+		mainigaisAttels.transform.localScale = new Vector2 (1F * pasreizejaVertiba, 1);
+	}
+}
